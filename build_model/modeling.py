b0a0ae0a95337e5d9a9d5deaad7b702d557ea4c5
Luthfi507 <luthfialbadar@gmail.com>
2026-05-12 23:46:56 +0700

    set tracking uri to localhost and fix project path

--- build_model/modeling.py
+++ build_model/modeling.py
@@ -3,10 +3,10 @@ import os
 import mlflow
 
 project_dir = os.path.dirname(__file__)
-data_dir = os.path.join(project_dir, '..', 'Egg-1')
-model = YOLO('yolo11m.pt')
+data_dir = os.path.join(project_dir, '..', 'dataset', 'Egg-1')
+model = YOLO('yolo11m.pt') 
 
-mlflow.set_tracking_uri("mlruns")
+mlflow.set_tracking_uri("http://127.0.0.1:5001")
 mlflow.set_experiment("Object-Counting-Experiment")
 
 # Define training parameters
@@ -16,14 +16,16 @@ imgsz = 640
 batch = 8
 
 def train():
-    with mlflow.start_run():
+    with mlflow.start_run(run_name='egg-counting'):
         model.train(
             data=data,
             epochs=epochs,
             imgsz=imgsz,
             batch=batch,
             device='cuda',
-            name='yolov11m-egg',
+            name='egg-counting',
+            project=os.path.join(project_dir, '..', 'monitoring', 'runs'),
+            exist_ok=True
         )
 
 if __name__ == "__main__":
