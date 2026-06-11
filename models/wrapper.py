975b41df9906e1c98bac02c26ad9a08388b2945f
Luthfi507 <luthfialbadar@gmail.com>
2026-06-11 22:41:42 +0700

    add params and simplify the output result

--- models/wrapper.py
+++ models/wrapper.py
@@ -1,18 +1,16 @@
 from ultralytics import YOLO
 import mlflow
-import pandas as pd
 
 class YOLOWrapper(mlflow.pyfunc.PythonModel):
     def load_context(self, context):
-        self.model = YOLO(context.artifacts['model_path'])
+        model_path = context.artifacts['model_path']
+        self.model = YOLO(model_path, task='detect')
+        print(f"Loaded model from {model_path}")
 
-    def predict(self, context, model_input):
-        preds = self.model(model_input)
-        pred_xyxy = pd.DataFrame(preds[0].boxes.xyxy.cpu().numpy(), columns=['xmin', 'ymin', 'xmax', 'ymax'])
-        pred_class = pd.DataFrame(preds[0].boxes.cls.cpu().numpy(), columns=['class']).astype(int)
-        pred_conf = pd.DataFrame(preds[0].boxes.conf.cpu().numpy(), columns=['confidence'])
-        pred_class['name'] = pred_class['class'].replace(preds[0].names)
-        return pd.concat([pred_xyxy, pred_conf, pred_class], axis=1)
+    def predict(self, model_input, params=None):
+        params = params or {}
+        preds = self.model.predict(model_input, **params)
+        return preds
     
 if __name__ == "__main__":
     class FakeContext:
@@ -20,9 +18,10 @@ if __name__ == "__main__":
             self.artifacts = artifacts
 
     wrapper = YOLOWrapper()
-    context = FakeContext({'model_path': '../monitoring/runs/weights/best.onnx'})
+    context = FakeContext({'model_path': '/media/duke/Data/deployment/runs/detect/car-counting/train/weights/best.onnx'})
     wrapper.load_context(context)
 
-    img_path = '../dataset/Egg-1/test/images/not_damaged_9_jpg.rf.bf5fa2b72bc716e9e2431a0756e7923a.jpg'
-    pred = wrapper.predict(context=None, model_input=img_path)
+    img_path = '/media/duke/Data/datasets/Car-1/test/images/IMG_0602_jpeg.rf.fdd986ef02f7e3c0086b989c11adc0bd.jpg'
+    params = {'imgsz': 224, 'conf': 0.25}
+    pred = wrapper.predict(model_input=img_path, params=params)
     print(pred)
